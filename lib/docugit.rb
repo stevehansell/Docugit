ab2296304a2905e90bd8c0520a3c11fa15bbd188
Steve Hansell <shansell@gannett.com>
2012-03-26 11:02:29 -0400

    Initial code

--- lib/docugit.rb
+++ lib/docugit.rb
@@ -1,3 +1,18 @@
+#! /usr/bin/env ruby
 $:.unshift File.dirname(__FILE__)
 
 require 'grit'
+
+class Docugit
+  attr_accessor :git_directory, :repo
+  
+  def initialize
+    @git_directory = Dir.pwd
+    @repo = Grit::Repo.new(@git_directory)
+  end
+  
+  def get_committed_files
+    master_commits = @repo.commits
+  end
+  
+end
